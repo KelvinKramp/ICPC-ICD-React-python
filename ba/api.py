e5519d5a86588a31780ed7c725d5d3aa3fb984c4
kkramp <kkramp@gericall.nl>
2022-06-25 15:05:34 +0200

    working api

--- ba/api.py
+++ ba/api.py
@@ -2,33 +2,34 @@
 from flask import Flask
 import datetime
 import json
-# get this object
-from flask import Response
 import datetime
-  
+from flask import jsonify
+
+
 x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
   
-  
-# Route for seeing a data
-@app.route('/data')
-def get_time():
-  
-    # Returning an api for showing in  reactjs
-    top100Films = [
+top100Films = [
   { "title": "The Shawshank Redemption", "year": 1994 },
   { "title": "The Godfather", "year": 1972 },
   { "title": "The Godfather: Part II", "year": 1974 },
   { "title": "The Dark Knight", "year": 2008 },
 ]
-    return {
-        'name':"geek", 
-        "age":"22",
-        "date":x, 
-        "programming":"python"
-        }
+# print(jsonify(top100Films))
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+  
+    # Returning an api for showing in  reactjs
+  return jsonify(top100Films)
+    # return {
+    #     'name':"Kelvin", 
+    #     "age":"22",
+    #     "date":x, 
+    #     "programming":"python"
+    #     }
      
 # Running app
 if __name__ == '__main__':
